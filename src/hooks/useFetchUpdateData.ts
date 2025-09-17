95beb4ef3375fc091f1f04b01c302f1c874dc39d
Abhilashghope <abhilashghope111@gmail.com>
2025-09-17 17:36:31 +0530

    force update enhanced, not waitin for an api to complete

--- src/hooks/useFetchUpdateData.ts
+++ src/hooks/useFetchUpdateData.ts
@@ -32,13 +32,13 @@ const useFetchUpdateData = () => {
     setError(null);
 
     try {
-          const data = await apiCall<ApiResponse>(
-            axiosInstance.get('/v3/appConfig', {
-              headers: {
-                Authorization: 'Basic cXZDYXN0bGVFbnRyeTpjYSR0bGVfUGVybWl0QDAx',
-              },
-            })
-          );
+      const data = await apiCall<ApiResponse>(
+        axiosInstance.get('/v3/appConfig', {
+          headers: {
+            Authorization: 'Basic cXZDYXN0bGVFbnRyeTpjYSR0bGVfUGVybWl0QDAx',
+          },
+        })
+      );
       // const data = {
       //   minVersion: '1.0.0',
       //   appStoreURL: 'https://itunes.apple.com/app/id1542005830',
